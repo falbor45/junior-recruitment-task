d03633592f3dcf41253e1c7c43f00e93ca916d4d
Artur Falborski <falborski.artur@gmail.com>
2018-09-28 12:50:57 +0200

    handle POST requests

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 const port = 3000;
 
 const mongoDB = 'mongodb://testuser:test123@ds115353.mlab.com:15353/to-do-junior-recruitment-task';
 
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 let db = mongoose.connection;
+// db.dropDatabase()
 db.on('error', console.error.bind(console, 'Connection error!'));
 db.once('open', function () {
 
@@ -30,9 +35,22 @@ app.route('/to-do-list/backend/task-list')
   .get(function (req, res) {
     Todo.find({}).exec(function (err, taskList) {
       if (err) {
-        res.status(500).send(err)
+        return res.status(500).send(err)
       }
-      res.status(200).send(taskList);
+      return res.status(200).send(taskList);
+    })
+  })
+  .post(function (req, res) {
+    if (typeof req.body.content !== "string") {
+      return res.status(400).send("Task content must be of String type!");
+    }
+    let freshTodo = new Todo({content: req.body.content, finished: false});
+
+    freshTodo.save(function (err) {
+      if (err) {
+        return res.status(500).send(err);
+      }
+      return res.status(200).send(freshTodo);
     })
   });
 
