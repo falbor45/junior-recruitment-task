eb58b4690ce83dc008e4aa5e3b7e1e043bdd6e2d
Artur Falborski <falborski.artur@gmail.com>
2018-09-28 12:22:45 +0200

    connect to mongoDB, handle GET requests for task list

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,39 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 const port = 3000;
+
+const mongoDB = 'mongodb://testuser:test123@ds115353.mlab.com:15353/to-do-junior-recruitment-task';
+
+mongoose.connect(mongoDB, { useNewUrlParser: true });
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Connection error!'));
+db.once('open', function () {
+
+});
+
+let todoSchema = new mongoose.Schema({
+  content: String,
+  finished: Boolean
+});
+
+let Todo = mongoose.model('Todo', todoSchema);
+
 
 app.use('/to-do-list/backend', function (req, res, next) {
   console.log(`Time: ${new Date()}`);
   next();
 });
 
-app.route('/to-do-list/backend')
+app.route('/to-do-list/backend/task-list')
   .get(function (req, res) {
-    res.send('Server is up and running!')
+    Todo.find({}).exec(function (err, taskList) {
+      if (err) {
+        console.log(err)
+      }
+      res.send(taskList);
+    })
   });
 
 app.listen(port, function () {
